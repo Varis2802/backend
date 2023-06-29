0d0e0c16339df0fedceb5dfbc09f8137b640170f
Varis2802 <varis.rana@parchaa.com>
2023-06-29 15:27:04 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS
+  // increase payload limit
+  app.use(bodyParser.json({ limit: '50mb' })); // adjust limit as needed
+
   await app.listen(4000);
 }
 bootstrap();
+
