408c3e31f762e45e4479cfe8d5aaf4ded17edd09
Varis2802 <varis.rana@parchaa.com>
2023-07-06 11:12:41 +0530

    commit final 1

--- src/connect-react/connect-react.controller.ts
+++ src/connect-react/connect-react.controller.ts
@@ -4,9 +4,9 @@ import * as fs from 'fs';
 
 @Controller('connect-react')
 export class ConnectReactController {
-    @Post('update-json')
-  updateJson(@Body() data: any, @Res() res: Response) {
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+  @Post('update-json')
+  updateJson(@Body('data') data: string, @Res() res: Response) {
+    fs.writeFile('updatedData.json', data, (err) => {
       if (err) {
         res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Error writing to file');
         throw err;
